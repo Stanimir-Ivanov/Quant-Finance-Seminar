6f1898cb11744a4173d818d6965f0347705bc432
Tommaso <tgarutti@gmail.com>
2018-03-20 17:38:08 +0100

    som mo changes

--- bigassfunction.R
+++ bigassfunction.R
@@ -43,25 +43,29 @@ VAR_estimation <- function(specifications, data_1)
   VAR_results <- rollapply(data_1, width = 1000,
                               FUN = function(data_1)
                               {
+                                #Estimate GARCH model and standirdized residuals
                                 results_data <- estimate_garch(specifications, data_1)
                                 mu <- results_data$forecast@forecast$seriesFor
                                 sigma <- results_data$forecast@forecast$sigmaFor
                                 innovations <- results_data$fit@fit$z
                                 
+                                #Estimate GPD distribution coefficients
                                 GPD_dist <- estimate_gpd(innovations)
                                 GPD_coef <- GPD_dist$results$par
                                 
+                                #Calculate quantile of the innovations
                                 q <- 0.95
                                 beta <- GPD_coef[1]
                                 xi <- GPD_coef[2]
                                 innovations_quantile = quantile(innovations, 0.899) + 
                                   (beta/xi)*(((1-q)*1000/100)^(-xi) -1)
                                 
+                                #Calculate the VAR
                                 VAR_onestep <- mu + sigma*innovations_quantile
                                 return(VAR_onestep)
                                 },
                       by.column=FALSE, align="right")
-  VAR_results  
+  return(VAR_results)  
 }
 ##----------------------------------------------------------------------------------------------------------##
 ##----------------------------------------------------------------------------------------------------------##
@@ -76,7 +80,8 @@ VAR_estimation <- function(specifications, data_1)
 #Define specifications of the GARCH model based on the input values.
 sp1 <-sp[1:1030];
 sp_garch_spec_t <- ugarchspec(variance.model = list(model = "sGARCH", garchOrder = c(1, 1)),
-                              mean.model = list(armaOrder = c(1, 0), include.mean = FALSE), distribution.model ="std")
+                              mean.model = list(armaOrder = c(1, 0), include.mean = FALSE), 
+                              distribution.model ="std")
 
 VaR_results <- VAR_estimation(specifications = sp_garch_spec_n, data_1 = sp1)
 ##----------------------------------------------------------------------------------------------------------##
