54015cfd39ff6f150a9172e6ca792ec2b21df4af
Stanimir Ivanov <stanimir.s.ivanov@gmail.com>
2018-03-20 12:07:05 +0100

    Cleaned aex data, changed read & transform script

--- read_and_transform.R
+++ read_and_transform.R
@@ -5,9 +5,8 @@ library(zoo)
 sp <- read.zoo("./Data/SP-500.csv", header = TRUE, sep = ",",format="%m/%d/%Y",index.column = 1)
 ing <- read.zoo("./Data/ING.csv", header = TRUE, sep = ",",format="%Y/%m/%d",index.column = 1)
 rds <- read.zoo("./Data/RDS-B.csv", header = TRUE, sep = ",",format="%Y/%m/%d",index.column = 1)
-aex <- read.zoo("./Data/AEX.csv", header = TRUE, sep = ",",format="%Y/%m/%d",index.column = 1)
-aex[aex=="null"] <- NA
-
+aex <- read.zoo("./Data/AEX.csv", header = TRUE, sep = ",",format="%m/%d/%Y",index.column = 1)
+# transform to xts
 sp <- xts(sp)
 ing <- xts(ing)
 aex <-xts(aex)
@@ -16,4 +15,5 @@ rds <- xts(rds)
 sp <- - 100*diff(log(sp))
 ing <- - 100*diff(log(ing))
 rds<- - 100*diff(log(rds))
-# aex<- - 100*diff(log(aex))
+aex<- - 100*diff(log(aex))
+ 
