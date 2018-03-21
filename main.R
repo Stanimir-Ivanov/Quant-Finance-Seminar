921b68384cb3107f23ad36e0c09f8aca62710483
Stanimir Ivanov <stanimir.s.ivanov@gmail.com>
2018-03-21 13:25:14 +0100

    Auto stash before merge of "master" and "origin/master"
    Moved GARCH specifications to a different file

--- main.R
+++ main.R
@@ -3,16 +3,16 @@
 ##----------------------------------------------------------------------------------------------------------##
 source("read_and_transform.R") # read data
 source("day_ahead_var.R") # read day ahead VaR functions
+source("garch_spec.R")
 
 library(rugarch)
 library(extRemes)
 
 sp1 <-sp[2:length(sp)];
+ing1 <- ing[2:length(ing)]
 #Define specifications of the GARCH model based on the input values.
-sp_garch_spec_t <- ugarchspec(variance.model = list(model = "sGARCH", garchOrder = c(1, 1)),
-                              mean.model = list(armaOrder = c(1, 0), include.mean = FALSE), 
-                              distribution.model ="norm")
 
-VaR_results <- day_ahead_VAR(specifications = sp_garch_spec_t, data_1 = sp1)
+VaR_results <- day_ahead_VAR(specifications = spec$garch_t, data_1 = sp1)
+VaR_results_ing <- day_ahead_VAR(specifications = spec$garch_t, data_1 = ing1)
 ##----------------------------------------------------------------------------------------------------------##
 ##----------------------------------------------------------------------------------------------------------##
