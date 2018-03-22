fbde81ce6d9cc0ede80d8af2588afc907a3a52e9
Tommaso <tgarutti@gmail.com>
2018-03-22 18:40:26 +0100

    H-day ahead VaR function, haven't tested yet

--- hday_ahead_VaR.R
+++ hday_ahead_VaR.R
@@ -5,11 +5,21 @@ library(SpatialExtremes)
 ##----------------------------------------------------------------------------------------------------------##
 ##Random innovation sumulation function.
 ##----------------------------------------------------------------------------------------------------------##
-rand_innovation <- function(z_vec, k_high, k_low)
+rand_innovation <- function(z_vec, gpd)
 {
   n_rand <- as.numeric(sample.int(1:1000, 1, replace = TRUE))
   z_rand <- z_vec[n]
   
+  if (z_rand > gpd$right_thresh)
+  {
+    z_rand <- gpd$right_thresh + rgpd(1, loc = 0, gpd$right_beta, gpd$right_xi)
+  }
+  if (z_rand < gpd$left_thresh)
+  {
+    z_rand <- left_thresh - rgpd(1, loc = 0, gpd$left_beta, gpd$left_xi)
+  }
+  z_vec[n] <- z_rand
+  return(list(z = z_rand, innovations = z_vec))
 }
 ##----------------------------------------------------------------------------------------------------------##
 ##----------------------------------------------------------------------------------------------------------##
@@ -31,32 +41,52 @@ hday_simulation_function <- function(specifications, data_1, q, results, h)
                              muFor <- results$muFor[counter]
                              sigmaFor <- results$sigmaFor[counter]
                              innovations <- results[counter, (length(results) - 999):length(results)]
-                             right_thres <- as.numeric(quantile(innovations,0.9))
-                             left_thres <- as.numeric(quantile(innovations,0.1))
                              
-                             right_beta <- results$right_tail_beta[counter]
-                             right_xi <- results$right_tail_xi[counter]
-                             left_beta <- results$left_tail_beta[counter]
-                             left_xi <- results$left_tail_xi[counter]
+                             gpd <- list(right_thresh = as.numeric(quantile(innovations,0.9)),
+                                         left_thresh = as.numeric(quantile(innovations,0.1)),
+                                         right_beta = results$right_tail_beta[counter],
+                                         right_xi = results$right_tail_xi[counter],
+                                         left_beta = results$left_tail_beta[counter],
+                                         left_xi = results$left_tail_xi[counter])
                              
-                             ar1_mu <- results$mu[counter]
-                             ar1_phi <- results$ar1[counter]
+                             garch <- list(ar1_mu = results$mu[counter],
+                                           ar1_phi = results$ar1[counter],
+                                           garch_mu = results$omega[counter],
+                                           garch_a = results$alpha1[counter],
+                                           garch_b = results$beta1[counter])
                              
-                             garch_mu <- results$omega[counter]
-                             garch_a <- results$alpha1[counter]
-                             garch_b <- results$beta1[counter]
+                             # Simulate random innovation
+                             z <- rand_innovation(innovations, gpd)
+                             innovations <- z$innovations
+                             xt1 <- muFor + sigmaFor*z$z
                              
-                             
-                             xt1 <- muFor + sigmaFor*
+                             # Define variables for hday-loop
+                             x <- xt1
+                             mu_old <- muFor
+                             sigma_old <- sigmaFor
                              
                              xt2_h <- lapply(1:(h-1),
                                              FUN <- function() 
                                              {
+                                               # Calculate next day variables
+                                               mu_new <- garch$ar1_phi*x
+                                               eps <- x - mu_old
+                                               sigma_new <- garch$garch_mu + garch$garch_a*eps
+                                                            + garch$garch_a*sigma_old
+                                               z <- rand_innovation(innovations, gpd)
+                                               innovations <- z$innovations
+                                               xt <- mu_new + sigma_new*z$z
                                                
+                                               # Define variables for next loop
+                                               x <- xt
+                                               mu_old <- mu_new
+                                               sigma_old <- sigma_new
+                                               
+                                               return(xt)
                                              })
                              
                              counter <<- counter + 1
-                             return()
+                             return(c(xt1, xt2_h))
                            },
                            by.column=FALSE, align="right")
   VaR_results <- lag(VaR_results)
