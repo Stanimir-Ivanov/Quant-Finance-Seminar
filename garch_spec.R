4d374da1fc47feb5f3c58352fc253b23de5bf77b
Stanimir Ivanov <stanimir.s.ivanov@gmail.com>
2018-03-22 14:26:21 +0100

    added tGARCH and eGARCH specifications

--- garch_spec.R
+++ garch_spec.R
@@ -1,10 +1,14 @@
-garch_spec_t <- ugarchspec(variance.model = list(model = "sGARCH", garchOrder = c(1, 1)),
-                              mean.model = list(armaOrder = c(1, 0), include.mean = FALSE), 
-                              distribution.model ="std")
-
 garch_spec_n <- ugarchspec(variance.model = list(model = "sGARCH", garchOrder = c(1, 1)),
-                              mean.model = list(armaOrder = c(1, 0), include.mean = FALSE), 
+                              mean.model = list(armaOrder = c(1, 0)), 
                               distribution.model ="norm")
 
-spec <- list(garch_t = garch_spec_t, garch_n = garch_spec_n)
+tgarch_spec_n <- ugarchspec(variance.model = list(model = "fGARCH", garchOrder = c(1, 1)),
+                           mean.model = list(armaOrder = c(1, 0), submodel = "TGARCH"), 
+                           distribution.model ="norm")
+
+egarch_spec_n <- ugarchspec(variance.model = list(model = "eGARCH", garchOrder = c(1, 1)),
+                            mean.model = list(armaOrder = c(1, 0)), 
+                            distribution.model ="norm")
+
+spec <- list(garch = garch_spec_n, tgarch = tgarch_spec_n, egarch = egarch__spec_n)
 
