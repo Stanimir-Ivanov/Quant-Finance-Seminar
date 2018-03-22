ba65820b03dcc6f09edfe43a98974158f75b953d
Tommaso <tgarutti@gmail.com>
2018-03-22 18:09:22 +0100

    Auto stash before merge of "master" and "origin/master"
    hday_simulation

--- hday_ahead_VaR.R
+++ hday_ahead_VaR.R
@@ -1,40 +1,62 @@
 source("two_step_method.R")
+library(SpatialExtremes)
+
+
+##----------------------------------------------------------------------------------------------------------##
+##Random innovation sumulation function.
+##----------------------------------------------------------------------------------------------------------##
+rand_innovation <- function(z_vec, k_high, k_low)
+{
+  n_rand <- as.numeric(sample.int(1:1000, 1, replace = TRUE))
+  z_rand <- z_vec[n]
+  
+}
+##----------------------------------------------------------------------------------------------------------##
+##----------------------------------------------------------------------------------------------------------##
+
+
+
+
 
 ##----------------------------------------------------------------------------------------------------------##
 ##Function to calculate VAR from GPD distribution and quantile of innovations z.
 ##----------------------------------------------------------------------------------------------------------##
-hday_simulation_function <- function(specifications, data_1, q, bigassmofo)
+hday_simulation_function <- function(specifications, data_1, q, results, h)
 {
-  hday_simulation <- rollapply(data_1, width = 1000,
-                           FUN = function(data_1)
+  counter <- 1
+  hday_simulation <- lapply(1:length(data_1),
+                           FUN = function()
                            {
-                             # Estimate GARCH model and standirdized residuals
-                             results_data <- estimate_garch(specifications, data_1)
-                             mu <- results_data$forecast@forecast$seriesFor
-                             sigma <- results_data$forecast@forecast$sigmaFor
-                             innovations <- results_data$fit@fit$z
+                             # Define parameters
+                             muFor <- results$muFor[counter]
+                             sigmaFor <- results$sigmaFor[counter]
+                             innovations <- results[counter, (length(results) - 999):length(results)]
+                             right_thres <- as.numeric(quantile(innovations,0.9))
+                             left_thres <- as.numeric(quantile(innovations,0.1))
                              
-                             xt1 <- mu + sigma*innovations
+                             right_beta <- results$right_tail_beta[counter]
+                             right_xi <- results$right_tail_xi[counter]
+                             left_beta <- results$left_tail_beta[counter]
+                             left_xi <- results$left_tail_xi[counter]
                              
-                             xt2_h <- lapply()
+                             ar1_mu <- results$mu[counter]
+                             ar1_phi <- results$ar1[counter]
                              
-                             cbind(xt a)
+                             garch_mu <- results$omega[counter]
+                             garch_a <- results$alpha1[counter]
+                             garch_b <- results$beta1[counter]
                              
-                             # Estimate GPD distribution coefficients
-                             GPD_dist <- estimate_gpd(innovations)
-                             GPD_coef <- GPD_dist$results$par
                              
-                             # Calculate quantile of the innovations
-                             beta <- GPD_coef['scale']
-                             xi <- GPD_coef['shape']
+                             xt1 <- muFor + sigmaFor*
                              
-                             # Calculate z_k+1
-                             z_k1 <- quantile(innovations, 0.899);
+                             xt2_h <- lapply(1:(h-1),
+                                             FUN <- function() 
+                                             {
+                                               
+                                             })
                              
-                             # Calculate VaR
-                             VaR_onestep <- calculate_VaR(z_k1, beta, xi, mu, sigma, q)
-                             
-                             return(VaR_onestep)
+                             counter <<- counter + 1
+                             return()
                            },
                            by.column=FALSE, align="right")
   VaR_results <- lag(VaR_results)
