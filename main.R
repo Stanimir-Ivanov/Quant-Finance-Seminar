4053dcf410249594cc42b97ef00e94ac784f8074
Stanimir Ivanov <stanimir.s.ivanov@gmail.com>
2018-03-29 15:36:40 +0200

    Auto stash before merge of "master" and "origin/master"
    Changed comments on main

--- main.R
+++ main.R
@@ -21,19 +21,19 @@ n <- 1000
 # save(res_dataName_specName, '.\Data\condi_evt\res_dataName_specName.RData')
 
 # sp
-res_sp_garch <- VaR_estimation(specifications = spec$garch, data_1 = sp_data, q, h, n) # done
+res_sp_garch <- VaR_estimation(specifications = spec$garch, data_1 = sp_data, q, h, n) # Stan
 res_sp_tgarch <- VaR_estimation(specifications = spec$tgarch, data_1 = sp_data, q, h, n) # Stan
 res_sp_egarch <- VaR_estimation(specifications = spec$egarch, data_1 = sp_data, q, h, n) # Stan
 
 # ing
 res_ing_garch <- VaR_estimation(specifications = spec$garch, data_1 = ing_data, q, h, n) # Stan
-res_ing_tgarch <- VaR_estimation(specifications = spec$tgarch, data_1 = ing_data, q, h, n) # Stan
+res_ing_tgarch <- VaR_estimation(specifications = spec$tgarch, data_1 = ing_data, q, h, n) # Tommaso
 res_ing_egarch <- VaR_estimation(specifications = spec$egarch, data_1 = ing_data, q, h, n) # Tommaso	
 
 # rds
 res_rds_garch <- VaR_estimation(specifications = spec$garch, data_1 = rds_data, q, h, n) # Tommaso
 res_rds_tgarch <- VaR_estimation(specifications = spec$tgarch, data_1 = rds_data, q, h, n) # Tommaso 
-res_rds_egarch <- VaR_estimation(sp0ecifications = spec$egarch, data_1 = rds_data, q, h, n) # Tommaso
+res_rds_egarch <- VaR_estimation(sp0ecifications = spec$egarch, data_1 = rds_data, q, h, n) # Thomas
 
 # aex
 res_aex_garch <- VaR_estimation(specifications = spec$garch, data_1 = aex_data, q, h, n) # Thomas
