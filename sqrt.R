9656c27139bfecb91c1e3ecca5ed13da06bc764d
Thomas <thanina@live.nl>
2018-03-29 15:18:27 +0200

    correct sqrt

--- sqrt.R
+++ sqrt.R
@@ -13,15 +13,15 @@ sqr_root <- function(h, var_data, data)  #var_data consists of the 1 day ahead V
   }
   return(violations)
 }
-#sqr_root(10, res_sp_garch$`1 Day 0.95`, sp1[1001:8406])                 643 violations
-#sqr_root(10, res_sp_garch$`1 Day 0.99`, sp1[1001:8406])                 203 violations
+#sqr_root(10, res_sp_garch$`1 Day 0.95`, sp1[1001:8406])               643 violations
+#sqr_root(10, res_sp_garch$`1 Day 0.99`, sp1[1001:8406])               203 violations
 
 hday_uncond <- function(h, var_data, data)  #var_data consists of the 10day ahead VaR 
 {
   violations = 0
   for (    i in 1:(length(data)-h)    ) {
     hdayreturn = sum(data[(i):(i+h-1)])
-    if (hdayreturn > var_data[i+h]) {violations = violations +1}
+    if (hdayreturn > var_data[i+h-1]) {violations = violations +1}
   }
   return(violations)
 }
