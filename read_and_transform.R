d95f23ec51263cb522859c1aced54560669e0e2c
Stanimir Ivanov <stanimir.s.ivanov@gmail.com>
2018-04-04 15:25:52 +0200

    added code to download gold data, removed ing

--- read_and_transform.R
+++ read_and_transform.R
@@ -4,24 +4,40 @@
 
 # Import data and convert into time series format
 sp <- read.zoo("./Data/SP-500.csv", header = TRUE, sep = ",",format="%m/%d/%Y",index.column = 1)
-ing <- read.zoo("./Data/ING.csv", header = TRUE, sep = ",",format="%Y/%m/%d",index.column = 1)
+# ing <- read.zoo("./Data/ING.csv", header = TRUE, sep = ",",format="%Y/%m/%d",index.column = 1)
 rds <- read.zoo("./Data/RDS-B.csv", header = TRUE, sep = ",",format="%Y/%m/%d",index.column = 1)
 aex <- read.zoo("./Data/AEX.csv", header = TRUE, sep = ",",format="%m/%d/%Y",index.column = 1)
+gold_raw <- Quandl("LBMA/GOLD", type = "xts")
+gold <- gold_raw$`GBP (PM`
+
 # transform to xts
 sp <- xts(sp)
-ing <- xts(ing)
+# ing <- xts(ing)
 aex <-xts(aex)
 rds <- xts(rds)
+
 # Negative log returns
 sp <- - 100*diff(log(sp))
-ing <- - 100*diff(log(ing))
+# ing <- - 100*diff(log(ing))
 rds<- - 100*diff(log(rds))
 aex<- - 100*diff(log(aex))
+gold <- - 100*diff(log(gold))
+
 # Data for use
 sp_data <- sp[2:length(sp)]
-ing_data <- ing[2:length(ing)]
+# ing_data <- ing[2:length(ing)]
 rds_data <- rds[2:length(rds)]
 aex_data <- aex[2:length(aex)]
 aex_data <- aex_data[!is.na(aex_data)]
+gold_data <- gold[!is.na(gold)]
+gold_data <- gold_data[time(gold_data) >= '1971-01-01']
+
+# Clean workspace
+remove(sp)
+remove(ing)
+remove(aex)
+remove(gold)
+remove(rds)
+remove(gold_raw)
 ##----------------------------------------------------------------------------------------------------------##
 ##----------------------------------------------------------------------------------------------------------##
